8583bab38724722009e381c952ea71b341313986
idstudent <sm1374@naver.com>
2025-06-30 19:17:05 +0900

    검색페이지 바텀시트 작업

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -5,6 +5,9 @@ import 'package:stamp_way_flutter/font_styles/app_text_style.dart';
 
 import '../model/tour_mapper.dart';
 
+final keywordProvider = StateProvider<String?>((ref) => null);
+final contentTypeIdProvider = StateProvider<int>((ref) => -1);
+
 class SearchPage extends ConsumerStatefulWidget {
   const SearchPage({super.key});
 
@@ -16,8 +19,31 @@ class _SearchPageState extends ConsumerState<SearchPage> {
   List<TourMapper> recentlySearchItem = [];
   List<TourMapper> nearSearchItem = [];
 
+  final _keywordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    final contentTypeId = ref.watch(contentTypeIdProvider);
+    String filterText = '검색할 분류를 선택해주세요';
+
+    switch(contentTypeId) {
+      case 12:
+        filterText = '여행지';
+        break;
+      case 14:
+        filterText = '문화';
+        break;
+      case 15:
+        filterText = '축제';
+        break;
+      case 28:
+        filterText = '액티비티';
+        break;
+      case 39:
+        filterText = '음식';
+        break;
+    }
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -30,13 +56,16 @@ class _SearchPageState extends ConsumerState<SearchPage> {
                 const SizedBox(height: 48,),
                 GestureDetector(
                   onTap: () {
-                    //TODO: 바텀시트
+                    _showFilterBottomSheet(context);
                   } ,
                   child: Row(
                     children: [
                       Icon(Icons.tune, color: AppColors.white, size: 24,),
                       const SizedBox(width: 8,),
-                      Text('검색할 분류를 선택해주세요', style: AppTextStyle.fontSize16WhiteRegular,)
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4),
+                        child: Text(filterText, style: AppTextStyle.fontSize16WhiteRegular,),
+                      )
                     ],
                   ),
                 ),
@@ -50,6 +79,7 @@ class _SearchPageState extends ConsumerState<SearchPage> {
                           borderRadius: BorderRadius.circular(24)
                         ),
                         child: TextFormField(
+                          controller: _keywordController,
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             enabledBorder: InputBorder.none,
@@ -100,6 +130,58 @@ class _SearchPageState extends ConsumerState<SearchPage> {
           ),
         )
       )
+    );
+  }
+
+  void _showFilterBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20)
+            )
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 12, left: 12, bottom: 12),
+                child: Text('필터', style: AppTextStyle.fontSize20WhiteRegular.copyWith(color: AppColors.black),),
+              ),
+              _bottomSheetFilterItem('여행지', 12),
+              _bottomSheetFilterItem('문화', 14),
+              _bottomSheetFilterItem('축제', 15),
+              _bottomSheetFilterItem('액티비티', 28),
+              _bottomSheetFilterItem('음식', 39),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _bottomSheetFilterItem(String title, int contentTypeId) {
+    return GestureDetector(
+      onTap: () {
+        ref.read(contentTypeIdProvider.notifier).state = contentTypeId;
+        Navigator.pop(context);
+      },
+      child: Container(
+        alignment: Alignment.centerLeft,
+        width: double.infinity,
+        padding: EdgeInsets.only(top: 12, bottom: 12, left: 12,),
+        child: Text(
+          title, 
+          style: AppTextStyle.fontSize20WhiteRegular.copyWith(color: AppColors.black),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 
