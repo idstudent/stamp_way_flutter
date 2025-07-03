5abd701e10abf9433da5c5203e46febf19df17c0
idstudent <sm1374@naver.com>
2025-07-03 16:42:08 +0900

    근처리스트 페이징 -> 상세페이지갔다가 오면 1페이지로오는거수정

--- lib/pages/near_place_list_page.dart
+++ lib/pages/near_place_list_page.dart
@@ -37,22 +37,22 @@ class _NearPlaceListPageState extends ConsumerState<NearPlaceListPage> {
     super.didChangeDependencies();
 
     typeId ??= GoRouterState.of(context).extra as int? ?? 12;
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (mounted) {
-        _checkLocationPermission();
-      }
-    });
   }
 
   @override
   void initState() {
     super.initState();
+    
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        _checkLocationPermission();
+      }
+    });
 
     _scrollController.addListener(() {
       if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent * 0.8) {
-        if (!isLoading && hasMore) {
-          _loadNextPage();
+        if (longitude != null && latitude != null) {
+          ref.read(getLocationProvider.notifier).loadNext(longitude!, latitude!, typeId!);
         }
       }
     });
@@ -60,43 +60,7 @@ class _NearPlaceListPageState extends ConsumerState<NearPlaceListPage> {
 
   @override
   Widget build(BuildContext context) {
-    ref.listen(getLocationProvider, (previous, next) {
-      print('getLocationProvider 상태 변경됨: ${next.runtimeType}');
-      next.when(
-        data: (newItems) {
-          print('데이터 받음: ${newItems.length}개, page: $page');
-          if(mounted) {
-            setState(() {
-              if (page == 1) {
-                allItems = List.from(newItems);
-              } else {
-                // 중복 제거
-                final item = newItems.where((newItem) {
-                  return !allItems.any((existingItem) =>
-                  existingItem.contentid == newItem.contentid);
-                }).toList();
-
-                allItems.addAll(item);
-
-                if (newItems.length < 20) {
-                  hasMore = false;
-                }
-              }
-              isLoading = false;
-            });
-          }
-        },
-        error: (error, stack) {
-          print('에러: $error');
-          if(mounted) {
-            setState(() {
-              isLoading = false;
-            });
-          }
-        },
-        loading: () {},
-      );
-    });
+    final locationData = ref.watch(getLocationProvider);
 
     return Scaffold(
       body: SafeArea(
@@ -118,50 +82,57 @@ class _NearPlaceListPageState extends ConsumerState<NearPlaceListPage> {
       ),
     );
   }
-
   Widget _getGridListView() {
-    if (allItems.isEmpty && isLoading) {
-      return Center(child: CircularProgressIndicator());
-    }
+    final locationData = ref.watch(getLocationProvider);
 
-    return GridView.builder(
-        controller: _scrollController,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisExtent: 370,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12,
-        ),
-        itemCount: allItems.length,
-        itemBuilder: (context, index) {
-          final location = allItems[index];
-
-          return NearTourItemWidget(
-            item: location,
-            itemClick: (item) {
-             context.pushNamed(AppRoutes.tourDetail, extra: location);
-            },
-            buttonClick: () {
-              ref.read(savedLocationProvider.notifier).saveTourLocation(location, (result) {
-                switch(result) {
-                  case Success():
-                    showToast(result.message);
-                    break;
-                  case Failure():
-                    showToast(result.message);
-                    break;
-                  case LoginRequired():
-                    showToast(result.message);
-                    context.pushNamed(AppRoutes.login);
-                    break;
-                  case MaxLimitReached():
-                    showToast(result.message);
-                    break;
-                }
-              });
-            },
-          );
+    return locationData.when(
+      data: (items) {
+        if (items.isEmpty) {
+          return Center(child: CircularProgressIndicator());
         }
+
+        return GridView.builder(
+            controller: _scrollController,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisExtent: 370,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+            ),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final location = items[index];
+
+              return NearTourItemWidget(
+                item: location,
+                itemClick: (item) {
+                  context.pushNamed(AppRoutes.tourDetail, extra: location);
+                },
+                buttonClick: () {
+                  ref.read(savedLocationProvider.notifier).saveTourLocation(location, (result) {
+                    switch(result) {
+                      case Success():
+                        showToast(result.message);
+                        break;
+                      case Failure():
+                        showToast(result.message);
+                        break;
+                      case LoginRequired():
+                        showToast(result.message);
+                        context.pushNamed(AppRoutes.login);
+                        break;
+                      case MaxLimitReached():
+                        showToast(result.message);
+                        break;
+                    }
+                  });
+                },
+              );
+            }
+        );
+      },
+      loading: () => Center(child: CircularProgressIndicator()),
+      error: (error, stack) => Center(child: Text('에러')),
     );
   }
 
@@ -175,33 +146,11 @@ class _NearPlaceListPageState extends ConsumerState<NearPlaceListPage> {
         latitude = position.latitude;
 
         if (mounted) {
-          setState(() {
-            page = 1;
-            allItems.clear();
-            hasMore = true;
-            isLoading = true;
-          });
-
-          ref.read(getLocationProvider.notifier).getLocationTourList(
-              longitude!, latitude!, page, typeId!);
+          ref.read(getLocationProvider.notifier).getLocationTourList(longitude!, latitude!, 1, typeId!);
         }
       }catch(e) {
         showToast('위치 정보를 가져올 수 없어요');
       }
-    }
-  }
-
-  void _loadNextPage() {
-    if (isLoading || !hasMore || !mounted) return;
-
-    if(mounted) {
-      setState(() {
-        isLoading = true;
-        page++;
-      });
-
-      ref.read(getLocationProvider.notifier).getLocationTourList(
-          longitude!, latitude!, page, typeId!);
     }
   }
 
--- lib/provider/get_location_provider.dart
+++ lib/provider/get_location_provider.dart
@@ -5,19 +5,48 @@ import 'package:stamp_way_flutter/provider/dio_provider.dart';
 final getLocationProvider = NotifierProvider<GetLocationProvider, AsyncValue<List<TourMapper>>>(GetLocationProvider.new);
 
 class GetLocationProvider extends Notifier<AsyncValue<List<TourMapper>>>{
+  List<TourMapper> _allItems = [];
+  int _currentPage = 1;
+  bool _hasMore = true;
+  bool _isLoading = false;
+
   @override
   AsyncValue<List<TourMapper>> build() {
+    ref.keepAlive(); // 이 한 줄만 추가
     return const AsyncValue.loading();
   }
 
-  Future<void> getLocationTourList(double longitude, double latitude, int pageNo, int contentTypeId,) async {
+  Future<void> getLocationTourList(double longitude, double latitude, int pageNo, int contentTypeId) async {
+    if (_isLoading) return;
+    _isLoading = true;
+
     try {
       final repository = ref.read(locationTourRepositoryProvider);
-
       final item = await repository.getLocationTourList(longitude, latitude, pageNo, contentTypeId);
-      state = AsyncValue.data(item);
+
+      if (pageNo == 1) {
+        _allItems = item;
+      } else {
+        _allItems.addAll(item.where((newItem) =>
+        !_allItems.any((existing) => existing.contentid == newItem.contentid)));
+      }
+
+      _currentPage = pageNo;
+      _hasMore = item.length >= 20;
+      state = AsyncValue.data(List.from(_allItems));
     } catch (e, stackTrace) {
       state = AsyncValue.error(e, stackTrace);
+    } finally {
+      _isLoading = false;
     }
   }
+
+  Future<void> loadNext(double longitude, double latitude, int contentTypeId) async {
+    if (!_isLoading && _hasMore) {
+      await getLocationTourList(longitude, latitude, _currentPage + 1, contentTypeId);
+    }
+  }
+
+  bool get isLoading => _isLoading;
+  bool get hasMore => _hasMore;
 }
